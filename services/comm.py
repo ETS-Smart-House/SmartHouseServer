72f4ea930cbbdee3cadcdbbe27957f24833cbf9c
Rastko D <60042153+RastkoDr@users.noreply.github.com>
2022-03-20 17:04:48 +0100

    set values

--- services/comm.py
+++ services/comm.py
@@ -3,13 +3,13 @@ from serial
 from services.temperature import get_current_temperature
 
 
-def sendCom(command, pin, inputvalue, port, baudrate, timeout)
+def sendCom(command, pin, inputvalue, port="/dev/ttyUSB0", baudrate=115200, timeout=0.1)
   arduino = serial.Serial(port, baudrate, timeout)
   strtuple = (command, str(pin), str(inputvalue))
   output = "-".join(strtuple)
   arduino.write(bytes(output, 'utf-8'))
 
-def inputCom(port, baudrate, timeout)
+def inputCom(port="/dev/ttyUSB0", baudrate=115200, timeout=0.1)
   arduino = serial.Serial(port, baudrate, timeout)
   data = arduino.readline()
   return data
