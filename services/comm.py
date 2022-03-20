737bb77348f1c98e65868aa2fd3ef7e14c787661
Rastko D <60042153+RastkoDr@users.noreply.github.com>
2022-03-20 15:15:08 +0100

    communication

--- services/comm.py
+++ services/comm.py
@@ -1,6 +1,15 @@
+import time
+from serial import Serial
 from services.temperature import get_current_temperature
 
 
+def sendCom(command, pin, inputvalue, port, baudrate, timeout)
+  arduino = serial.Serial(port, baudrate, timeout)
+  strtuple = (command, str(pin), str(inputvalue))
+  output = "-".join(strtuple)
+  arduino.write(bytes(output, 'utf-8'))
 
-
-
+def inputCom(port, baudrate, timeout)
+  arduino = serial.Serial(port, baudrate, timeout)
+  data = arduino.readline()
+  return data
